fac75579b48b9cc793f7f203049daa1caee0c417
DoheumPark <microform@korea.com>
2022-01-08 12:48:23 +0900

    회원가입 백엔드 작업 완료

--- src/main/java/com/koreait/springweb/user/UserController.java
+++ src/main/java/com/koreait/springweb/user/UserController.java
@@ -1,15 +1,28 @@
 package com.koreait.springweb.user;
 
+import com.koreait.springweb.model.UserEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping("/join")
     public String join() {
         return "user/join";
+    }
+
+    @PostMapping("/join")
+    public String joinProc(UserEntity entity) {
+        int result = service.join(entity);
+
+        return "redirect:/user/login";
     }
 }
--- src/main/java/com/koreait/springweb/user/UserService.java
+++ src/main/java/com/koreait/springweb/user/UserService.java
@@ -1,7 +1,21 @@
 package com.koreait.springweb.user;
 
+import com.koreait.springweb.model.UserEntity;
+import org.mindrot.jbcrypt.BCrypt;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+
+    @Autowired
+    private UserMapper mapper;
+
+    public int join(UserEntity entity) {
+        //비밀번호 암호화
+        String hashedPw = BCrypt.hashpw(entity.getUserpw(), BCrypt.gensalt());
+        entity.setUserpw(hashedPw);
+
+        return mapper.insUser(entity);
+    }
 }
