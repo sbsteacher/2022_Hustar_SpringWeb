bb21fe8a25e35b20aad18b0e5bb9eb76950c2dfa
DoheumPark <microform@korea.com>
2022-01-15 11:34:57 +0900

    Board 세팅

---
+++ src/main/java/com/koreait/springweb/board/BoardMapper.java
@@ -0,0 +1,7 @@
+package com.koreait.springweb.board;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface BoardMapper {
+}
---
+++ src/main/java/com/koreait/springweb/board/BoardService.java
@@ -0,0 +1,7 @@
+package com.koreait.springweb.board;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BoardService {
+}
