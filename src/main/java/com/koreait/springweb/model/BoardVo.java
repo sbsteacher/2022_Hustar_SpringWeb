9de2b48664a4d8b54f126de40601879ef7cd3911
DoheumPark <microform@korea.com>
2022-01-22 10:02:13 +0900

    BoardVo

---
+++ src/main/java/com/koreait/springweb/model/BoardVo.java
@@ -0,0 +1,10 @@
+package com.koreait.springweb.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BoardVo extends BoardEntity {
+    private String writernm;
+}
